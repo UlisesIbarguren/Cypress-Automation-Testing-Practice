1afc934326a08dad6f99e2cec8f6a42d92d27428
ulisesibarguren <ulises.ibarguren@gmail.com>
2024-07-23 12:39:14 -0300

    Fixed POM for ask question test

--- cypress/e2e/askQuestion.cy.js
+++ cypress/e2e/askQuestion.cy.js
@@ -1,12 +1,23 @@
 import EcommercePage from "../pageObjects/ecommerce";
 
 describe('template spec', () => {
-  const ecommercePage = new EcommercePage()
-  it('Adds a Product to the Whishlist', () => {
-    const baseUrl = Cypress.env('baseUrl')
+  const ecommercePage = new EcommercePage();
+
+  it('Adds a Product to the Wishlist', () => {
+    const baseUrl = Cypress.env('baseUrl');
     cy.visit(baseUrl);
+    
     ecommercePage.searchProduct('MacBook Pro');
-    ecommercePage.selectProduct()
-    ecommercePage.askQuestion('John Doe', 'john.doe@example.com', 'Product Inquiry', 'I would like to know more about the product.')
-  })
-})
+    ecommercePage.selectProduct();
+
+    // Define the test data
+    const testData = {
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      subject: 'Product Inquiry',
+      enquiry: 'I would like to know more about the product.'
+    };
+
+    ecommercePage.askQuestion(testData.name, testData.email, testData.subject, testData.enquiry);
+  });
+});
